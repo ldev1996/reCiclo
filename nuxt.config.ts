738a4cb0095e2c4c7ac4b97ed42c10e292517c2a
ldev1996 <leonardo.dev1996@gmail.com>
2025-04-13 18:35:51 -0300

    PWA config, favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,29 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
+  modules: ["@vite-pwa/nuxt"],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'ECOmunidade',
+      short_name: 'ECO',
+      description: 'Troque e recicle com sua comunidade',
+      theme_color: '#00C950', // green-500
+      background_color: '#F9FAFB', // gray-50
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  }
 });
