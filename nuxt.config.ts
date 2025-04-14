671a0d55a2192240d623753a988be7d89e4b26d2
ldev1996 <leonardo.dev1996@gmail.com>
2025-04-14 13:50:19 -0300

    fallback title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,7 +40,8 @@ export default defineNuxtConfig({
     head: {
       link: [
         { rel: 'manifest', href: '/manifest.webmanifest' }
-      ]
+      ],
+      title: "ECOmunidade"
     }
   }
 });
