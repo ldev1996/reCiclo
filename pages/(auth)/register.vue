68f9d886bc34a011010d864ff9a597c09864d279
ldev1996 <leonardo.dev1996@gmail.com>
2025-04-18 17:20:11 -0300

    updated signuo styled form

--- pages/(auth)/register.vue
+++ pages/(auth)/register.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="flex flex-col items-center space-y-4">
-      <form class="w-full flex flex-col gap-4 px-4 pt-4">
+      <form method="post" class="w-full flex flex-col gap-4 px-4 pt-4">
         <input
-          type="email"
+          type="text" required
+          name="username"
+          id="username"
+          placeholder="Seu nome completo"
+        />
+        <input
+          type="email" required
           name="email"
           id="email"
           placeholder="Seu e-mail"
@@ -10,10 +16,16 @@
         />
         <p class="hidden peer-invalid:block text-error">Por favor, forneça um endereço de e-mail válido.</p>
         <input
-          type="password"
+          type="password" required
           name="password"
           id="password"
           placeholder="Senha"
+        />
+        <input
+          type="password" required
+          name="confirm"
+          id="confirm"
+          placeholder="Confirme sua senha"
         />
         <button type="submit"
         class="bg-primary hover:bg-primary-dark text-white px-5 py-2 rounded-xl shadow-md transition">
@@ -25,7 +37,7 @@
   
 <script setup lang="js">
     definePageMeta({
-        layout: "auth",
+        layout: "auth"
     });
 </script>
   
